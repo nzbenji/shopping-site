c67a838c4036a1f43c4a57ad92e5364e014aa22b
Ben <benlittle53@gmail.com>
2018-12-05 11:05:45 +1300

    Items are now being added to cart

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,8 @@ import { items } from './components/Dummy-Data.js';
 
 class App extends React.Component {
   state = {
-    activeTab: 0
+    activeTab: 0,
+    cart: []
   };
 
   //Choose wether to render ItemPage or CartPage
@@ -16,12 +17,22 @@ class App extends React.Component {
     })
   }
 
+  handleAddToCart = item => {
+    this.setState({
+      //Copying the cart state using spread operator, plus one new item
+      cart: [...this.state.cart, item.id]
+    })
+  }
+
   renderContent() {
     //Switch between item and cart tabs
     switch(this.state.activeTab) {
       default:
       case 0: return (
-        <ItemPage items={ items } />
+        <ItemPage 
+        items={ items } 
+        addToCart= { this.handleAddToCart }
+        />
       )
       case 1: return (
         <span>Cart</span>
@@ -32,6 +43,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+      <div>{this.state.cart.length} items</div>
         <Nav activeTab = { this.state.activeTab } changeTab = { this.changeTab }/>
         <main className="App__content">
           {this.renderContent()}
