0184de26ec8a6df44dcb4488ae0777647a449436
nzbenji <benlittle53@gmail.com>
2019-03-15 14:31:36 +1300

    mongodb and server setup for auth

--- server/server.js
+++ server/server.js
@@ -1,19 +1,27 @@
 const cors = require('cors')
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const router = require('./routes')
-
 const app = express()
-const port = 8080
 
+//App setup
+app.use(morgan('combined'))
+app.use(bodyParser.json({type: '*/*'}))
 app.use(cors())
 app.use('/api', router)
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.listen(port, () => {
+
+//Server setup.
+const port = process.env.PORT || 8080
+const server = http.createServer(app)
+server.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('Server is listening on port', port)
 })
